88e87c4886ed16f7849ae3b2792c88b5b9aaf7f3
Seepo <seep0@pm.me>
2026-05-30 11:18:24 +0300

    adjust for kitten

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -4,6 +4,7 @@ set -xeuo pipefail
 
 # Remove subscription-manager, install EPEL and enable CRB
 #dnf -y remove subscription-manager
+dnf -y install 'dnf-command(config-manager)
 dnf config-manager --set-enabled crb
 dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm
 
