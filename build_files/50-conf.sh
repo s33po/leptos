d2f5caeb993aeb6fe21f3e2e1b02c69e47b25869
s33po <seep0@pm.me>
2026-06-19 15:15:57 +0300

    add firewalld zone

--- build_files/50-conf.sh
+++ build_files/50-conf.sh
@@ -18,3 +18,25 @@ sed -i 's|^LookAndFeelPackage=.*|LookAndFeelPackage=org.kde.breezedark.desktop|'
 
 sed -i 's|^ColorScheme=.*|ColorScheme=BreezeDark|' \
   /usr/share/kde-settings/kde-profile/default/xdg/kdeglobals
+
+# Write firewalld zone "Workstation" (more permissive than stock)
+mkdir -p /usr/lib/firewalld/zones
+cat > /usr/lib/firewalld/zones/Workstation.xml <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<zone>
+  <short>Workstation</short>
+  <description>Unsolicited incoming network packets are rejected from port 1 to 1024,
+except for select network services. Incoming packets that are related to
+outgoing network connections are accepted. Outgoing network connections are
+allowed.</description>
+  <service name="dhcpv6-client"/>
+  <service name="ssh"/>
+  <service name="samba-client"/>
+  <port protocol="udp" port="1025-65535"/>
+  <port protocol="tcp" port="1025-65535"/>
+  <forward/>
+</zone>
+EOF
+
+# Set default firewalld zone to "Workstation"
+firewall-offline-cmd --set-default-zone=Workstation
