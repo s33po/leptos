479c0c2da69c64af1bd147e1b2c41de63ef1e106
Seepo <seep0@pm.me>
2026-08-22 15:39:01 +0300

    test

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -7,47 +7,28 @@ dnf -y install 'dnf-command(config-manager)' epel-release
 dnf config-manager --set-enabled crb
 dnf -y upgrade epel-release
 
-# Set global dnf options
-dnf config-manager --save \
-    --setopt=max_parallel_downloads=10
-
 # Install packages
 dnf -y install --setopt=install_weak_deps=False \
     cifs-utils \
     container-tools \
     distrobox \
-    firewalld \
     git-core \
     lshw \
     make \
-    qemu-guest-agent \
-    rsync \
     system-reinstall-bootc \
     systemd-container \
-    systemd-resolved \
     time \
-    tmux \
-    toolbox \
     tree \
-    tuned-ppd \
     usbutils \
-    lsof \
-    bind-utils \
-    xfsdump \
-    vim-enhanced \
-    vim-common \
-    wget
-
-# Preset and enable resolved
-tee /usr/lib/systemd/system-preset/91-resolved-default.preset > /dev/null <<'EOF'
-enable systemd-resolved.service
-EOF
-
-tee /usr/lib/tmpfiles.d/resolved-default.conf > /dev/null <<'EOF'
-L /etc/resolv.conf - - - - ../run/systemd/resolve/stub-resolv.conf
-EOF
-
-systemctl preset systemd-resolved.service
+    wget \
+    btop \
+    fastfetch \
+    fzf \
+    gum \
+    just \
+    nvtop \
+    podman-compose \
+    steam-devices
 
 # Set quiet bootc updates
 sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' \
