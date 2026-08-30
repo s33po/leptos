51c119ff127d787136b16fc1f6cf3d83f12e7500
Seepo <seep0@pm.me>
2026-08-30 14:26:19 +0300

    remove firewalld and resolved

--- build_files/60-services.sh
+++ build_files/60-services.sh
@@ -4,10 +4,8 @@ set -xeuo pipefail
 
 # Enable services
 systemctl enable bootc-fetch-apply-updates.timer
-systemctl enable firewalld.service
 systemctl enable plasmalogin.service
 systemctl enable podman.socket
-systemctl enable systemd-resolved.service
 systemctl enable tuned.service
 
 # Disable services
