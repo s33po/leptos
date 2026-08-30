dcc7c1283da4b31c1da79fd8425ea1bf94fe5196
Seepo <seep0@pm.me>
2026-08-30 14:25:32 +0300

    remove firewalld config

--- build_files/50-conf.sh
+++ build_files/50-conf.sh
@@ -8,24 +8,3 @@ install -Dm644 /build_files/user.just /usr/share/just/user.just
 # Create global alias for user.just commands
 echo "alias jmain='just --justfile /usr/share/just/user.just'" > /etc/profile.d/jmain.sh
 chmod 644 /etc/profile.d/jmain.sh
-
-# Write firewalld zone "Workstation" (more permissive than stock)
-mkdir -p /usr/lib/firewalld/zones
-cat > /usr/lib/firewalld/zones/Workstation.xml <<EOF
-<?xml version="1.0" encoding="utf-8"?>
-<zone>
-  <short>Workstation</short>
-  <description>Unsolicited incoming network packets are rejected from port 1 to 1024,
-except for select network services. Incoming packets that are related to
-outgoing network connections are accepted. Outgoing network connections are
-allowed.</description>
-  <service name="dhcpv6-client"/>
-  <service name="samba-client"/>
-  <port protocol="udp" port="1025-65535"/>
-  <port protocol="tcp" port="1025-65535"/>
-  <forward/>
-</zone>
-EOF
-
-# Set default firewalld zone to "Workstation"
-firewall-offline-cmd --set-default-zone=Workstation
